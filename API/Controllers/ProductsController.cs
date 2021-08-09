6e560cc291926909e9a160d52787fdd0c97243cc
Wahib <wahib1900@gmail.com>
2021-08-09 08:49:15 +0800

    Error Handling in API Completed

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,12 +9,12 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using API.Errors;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ProductsController : ControllerBase
+  public class ProductsController : BaseApiController
   {
     private readonly IGenericRepository<Product> _productRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -40,10 +40,13 @@ namespace API.Controllers
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
       var product = await _productRepo.GetEntityWithSpec(spec);
+      if (product == null) return NotFound(new ApiResponse(404));
       return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
 
     }
---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,11 @@
+namespace API.Errors
+{
+  public class ApiException : ApiResponse
+  {
+    public ApiException(int statusCode, string message = null, string detail = null) : base(statusCode, message)
+    {
+      Details = detail;
+    }
+    public string Details { get; set; }
+  }
+}
