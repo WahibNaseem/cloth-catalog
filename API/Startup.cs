641c586125ab417e5f7a79656372a495f15e20ab
Wahib <wahib1900@gmail.com>
2021-08-19 11:53:18 +0800

    added specification , pagination

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,14 @@ namespace API
 
       services.AddApplicationServices();
       services.AddSwaggerDocumentation();
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+        });
+
+      });
 
 
     }
@@ -53,6 +61,9 @@ namespace API
 
       app.UseRouting();
       app.UseStaticFiles();
+
+      app.UseCors("CorsPolicy");
+
 
       app.UseAuthorization();
       app.UseSwaggerDocumentation();
